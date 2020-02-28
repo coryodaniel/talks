47e54cb627e1d2274d38f1b8962d2ee1dab1c786
Cory O'Daniel <cory@coryodaniel.com>
2020-02-27 22:36:46 -0800

    wip

--- beam-n-k8s/code/better_together/lib/better_together/prime_calculators/primes_worker.ex
+++ beam-n-k8s/code/better_together/lib/better_together/prime_calculators/primes_worker.ex
@@ -16,19 +16,20 @@ defmodule BetterTogether.PrimeCalculators.PrimesWorker do
               max_prime: nil
   end
 
-  def start_link(limit) do
-    Logger.info("start_link: #{inspect(limit)}")
-    GenServer.start_link(__MODULE__, %State{limit: limit})
-  end
+  def start_link(limit), do: GenServer.start_link(__MODULE__, %State{limit: limit})
 
   @doc """
   Check the results of a `PrimesWorker`
   """
   def results(pid), do: GenServer.call(pid, :results)
 
+  ###################################################
+  ###
   ### Server
+  ###
+  ###################################################
+
   def init(%State{} = state) do
-    Logger.info("init: #{inspect(state)}")
     Process.flag(:trap_exit, true)
 
     Task.async(Primes, :get_primes_list, [state.limit])
@@ -39,30 +40,33 @@ defmodule BetterTogether.PrimeCalculators.PrimesWorker do
   def handle_call(:results, _from, state), do: {:reply, state, state}
 
   # Handoff initiated
-  def handle_call({:swarm, :begin_handoff}, _from, state) do
-    # TODO: if received SIGTERM, then handoff, else ignore
-    Logger.info("begin_handoff: #{inspect(state)}")
-    {:reply, {:resume, state}, state}
-  end
+  # TODO: if received SIGTERM, then handoff, else ignore  
+  def handle_call({:swarm, :begin_handoff}, _from, state), do: {:reply, {:resume, state}, state}
 
   # This is triggered whenever a process has been restarted on a new node.
-  def handle_cast({:swarm, :end_handoff, incoming_state}, _current_state) do
-    Logger.info("end_handoff: #{inspect(incoming_state)}")
-    # TODO: should restart the Talsk if it wasn't complete
-    {:noreply, incoming_state}
-  end
+  # TODO: should restart the Task if it wasn't complete  
+  def handle_cast({:swarm, :end_handoff, incoming_state}, _current_state),
+    do: {:noreply, incoming_state}
 
   # called when a netsplit is resolved
-  def handle_cast({:swarm, :resolve_conflict, incoming_state}, _current_state) do
-    Logger.info("resolve_conflict: #{inspect(incoming_state)}")
-    {:noreply, incoming_state}
-  end
+  def handle_cast({:swarm, :resolve_conflict, incoming_state}, _current_state),
+    do: {:noreply, incoming_state}
 
   # Handles calculation results from `Task` in `init/1`
-  def handle_info({_task, primes}, %State{} = state) when is_list(primes) do
+  def handle_info({_task, primes}, %State{} = state) when is_list(primes),
+    do: {:noreply, state_with_primes(state, primes)}
+
+  # Called when process should die because it is being moved
+  def handle_info({:swarm, :die}, state), do: {:stop, :shutdown, state}
+
+  def handle_info(_, state), do: {:noreply, state}
+
+  def terminate(_reason, state), do: Swarm.Tracker.handoff(__MODULE__, state)
+
+  defp state_with_primes(%State{} = state, primes) do
     ended_at = Time.utc_now()
 
-    new_state = %State{
+    %State{
       state
       | elapsed_time: Time.diff(ended_at, state.started_at, :microsecond),
         ended_at: ended_at,
@@ -70,19 +74,5 @@ defmodule BetterTogether.PrimeCalculators.PrimesWorker do
         num_primes: length(primes),
         max_prime: List.last(primes)
     }
-
-    {:noreply, new_state}
-  end
-
-  # Called when process should die because it is being moved
-  def handle_info({:swarm, :die}, state) do
-    {:stop, :shutdown, state}
-  end
-
-  def handle_info(_, state), do: {:noreply, state}
-
-  def terminate(_reason, state) do
-    IO.puts "terminate: #{inspect(state)}"
-    Swarm.Tracker.handoff(__MODULE__, state)
   end
 end
